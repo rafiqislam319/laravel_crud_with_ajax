3945c474cccf7f83ccb2d26b7a07a50eac6dd371
rafiqislam319 <rafiqislam319@gmail.com>
2023-11-03 19:50:46 +0600

    toaster message show after delete product

--- resources/views/product_js.blade.php
+++ resources/views/product_js.blade.php
@@ -1,6 +1,7 @@
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
 
 <script src="https://code.jquery.com/jquery-3.7.1.min.js" integrity="sha256-/JqT3SQfawRcv/BIHPThkBvs0OEvtFFmqPF/lYI/Cxo=" crossorigin="anonymous"></script>
+<script src="http://cdn.bootcss.com/toastr.js/latest/js/toastr.min.js"></script>
 <script>
     $.ajaxSetup({
         headers: {
@@ -94,6 +95,26 @@
                     success: function(response) {
                         if (response.status == 'success') {
                             $('#productTable').load(location.href + ' #productTable')
+
+                            Command: toastr["success"]("product deleted successfully")
+
+                            toastr.options = {
+                                "closeButton": false,
+                                "debug": false,
+                                "newestOnTop": false,
+                                "progressBar": true,
+                                "positionClass": "toast-top-right",
+                                "preventDuplicates": false,
+                                "onclick": null,
+                                "showDuration": "300",
+                                "hideDuration": "1000",
+                                "timeOut": "5000",
+                                "extendedTimeOut": "1000",
+                                "showEasing": "swing",
+                                "hideEasing": "linear",
+                                "showMethod": "fadeIn",
+                                "hideMethod": "fadeOut"
+                            }
                         }
                     }
                 });
--- resources/views/products.blade.php
+++ resources/views/products.blade.php
@@ -10,7 +10,7 @@
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <link rel="stylesheet" href="https://maxst.icons8.com/vue-static/landings/line-awesome/line-awesome/1.3.0/css/line-awesome.min.css">
-
+    <link rel="stylesheet" href="http://cdn.bootcss.com/toastr.js/latest/css/toastr.min.css">
     <title>Ajax Crud</title>
 </head>
 
@@ -40,19 +40,11 @@
                             <td>{{ $product->name }}</td>
                             <td>{{ $product->price }}</td>
                             <td>
-                                <a href="" class="btn btn-warning updateProductForm"
-                                 data-bs-toggle="modal"
-                                 data-bs-target="#updateProductModal"
-                                 data-id="{{$product->id}}" 
-                                 data-name="{{$product->name}}" 
-                                 data-price="{{$product->price}}"
-                                 >
-                                <i class="las la-edit">Edit</i></a>
+                                <a href="" class="btn btn-warning updateProductForm" data-bs-toggle="modal" data-bs-target="#updateProductModal" data-id="{{$product->id}}" data-name="{{$product->name}}" data-price="{{$product->price}}">
+                                    <i class="las la-edit">Edit</i></a>
 
-                                <a href="" class="btn btn-danger deleteProduct"
-                                data-id="{{$product->id}}"
-                                >
-                                <i class="las la-trash"></i>Delete</i></a>
+                                <a href="" class="btn btn-danger deleteProduct" data-id="{{$product->id}}">
+                                    <i class="las la-trash"></i>Delete</i></a>
                             </td>
                         </tr>
                         @endforeach
@@ -70,6 +62,8 @@
 
 
     @include('product_js')
+    
+    {!! Toastr::message() !!}
 
 
 </body>
